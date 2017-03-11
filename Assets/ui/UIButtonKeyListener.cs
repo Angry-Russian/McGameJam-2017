4b73ffc5008ca9fa2490a010a92ba6eea67571bc
Dmitri K <kolytchev.d@gmail.com>
2017-03-11 17:23:15 -0500

    adding ui behaviours

---
+++ Assets/ui/UIButtonKeyListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
+public class UIButtonKeyListener : MonoBehaviour {
+
+    public string inputName;
+
+    private Image sprite;
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<Image>();
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton(inputName)) 
+            this.transform.position = startPos + Vector3.down * 1f;
+        else
+            this.transform.position = startPos;
+    }
+}
